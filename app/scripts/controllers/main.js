a0b7a801c348c63e43ba6f5310bae56ecefcf0db
Michael Quinlan <mquinlan@sportslabs.com>
2013-12-12 15:18:55 -0700

    master: Making this game awesome

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,55 @@
 'use strict';
 
 angular.module('powerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $timeout) {
+
+    $scope.backgroundColor = '';
+
+    function _zeroPad(number){
+      return ('0'+number).slice(-2);
+    }
+
+    $scope.displayTime = function(){
+      var sec = parseInt($scope.seconds);
+
+      $scope.currentTime = Math.floor(sec/60) + ':' + _zeroPad(sec % 60);
+    };
+
+    $scope.toggleRunning = function(){
+      if($scope.isRunning){
+        $scope.isRunning = false;
+      }else{
+        $scope.isRunning = true;
+        $scope.run();
+      }
+    };
+
+    $scope.getRandomColor = function(){
+      return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    };
+
+    $scope.crazyBackground = function(){
+      if(parseInt($scope.seconds) % 60 < 4){
+        $scope.backgroundColor = $scope.getRandomColor();
+        $timeout($scope.crazyBackground, 100);
+      }else{
+        $scope.backgroundColor = '';
+      }
+    };
+
+    $scope.run = function(){
+      if($scope.isRunning && $scope.seconds > 0){
+        console.log(parseInt($scope.seconds % 60) === 3);
+        $scope.seconds--;
+        if(parseInt($scope.seconds % 60) === 3){
+          $scope.crazyBackground();
+          $scope.currentTime = 'DRINK';
+        }else{
+          $scope.displayTime();
+        }
+        $timeout($scope.run, 1000);
+      }
+    };
+    $scope.seconds = 3600;
+    $scope.displayTime();
   });
